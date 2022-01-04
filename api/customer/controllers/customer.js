eedd3ed8dd89e460c341cf1f20c836761f0c3132
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-04 09:48:47 +0700

    something

--- api/customer/controllers/customer.js
+++ api/customer/controllers/customer.js
@@ -2,16 +2,43 @@
 require("dotenv").config();
 const Response = require(`../../../utils/response`);
 
+const findOneByEmail = async (ctx) => {
+  const { email } = ctx.request.body;
+  try {
+    const user = await strapi.services.customer.findOneEmail(email);
+    if (user) {
+      return Response.ok(ctx, { data: user.id, msg: "OK", status: 200 });
+    }
+    return Response.badRequest(ctx, {
+      status: 400,
+      msg: `${email} does not exist`,
+    });
+  } catch (error) {
+    console.log(error);
+    return Response.internalServerError(ctx, {
+      status: 500,
+      msg: "Error in Server",
+    });
+  }
+};
+
 const logIn = async (ctx) => {
   const { email, password } = ctx.request.body;
   const user = await strapi
     .query(`customer`)
-    .findOne({ email, password }, ["customer.email"]);
+    .findOne({ email, password }, ["customer.email", "customer.id"]);
   if (user) {
-    return Response.ok(ctx, { data: user.email, msg: `OK`, status: 1 });
+    let data = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      gender: user.gender,
+      dateOfBirth: user.dateOfBirth,
+      phoneNumber: user.phoneNumber,
+    };
+    return Response.ok(ctx, { data: data, msg: `OK`, status: 1 });
   }
-  console.log(`email: ${email}, password: ${password} already logged in`);
-  return Response.notFound(ctx, {
+  return Response.ok(ctx, {
     data: null,
     msg: `User account or password incorrect`,
     status: 0,
@@ -33,9 +60,12 @@ const signUp = async (ctx) => {
     .create({ username, email, password, gender, dateOfBirth, phoneNumber });
   if (user) {
     let data = {
+      id: user.id,
       username: user.username,
       email: user.email,
-      id: user.id,
+      gender: user.gender,
+      dateOfBirth: user.dateOfBirth,
+      phoneNumber: user.phoneNumber,
     };
     return Response.created(ctx, {
       data: data,
@@ -50,7 +80,78 @@ const signUp = async (ctx) => {
   });
 };
 
+const resetPassWord = async (ctx) => {
+  const { email, otp } = ctx.request.body;
+  try {
+    const user = await strapi.services.customer.findOneEmail(email);
+    if (user) {
+      const OTP = strapi.services.customer.generateOTP();
+      try {
+        await strapi.services.customer.updateOTP(email, OTP);
+        strapi.services.email.send(
+          process.env.user,
+          email,
+          "Code Verification",
+          `Your password reset otp is ${OTP}`
+        );
+        return Response.ok(ctx, {
+          status: 200,
+          msg: `Successfully`,
+          data: email,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return Response.badRequest(ctx, {
+      status: 400,
+      msg: "Not Found",
+    });
+  } catch (error) {
+    console.log(error);
+    return Response.internalServerError(ctx, {
+      status: 500,
+      msg: "Error Server",
+    });
+  }
+};
+
+const changePassword = async (ctx) => {
+  const { email, password, newPassword } = ctx.request.body;
+  let user = null;
+  try {
+    user = await strapi.services.customer.checkLogin(email, password);
+  } catch (error) {
+    return Response.internalServerError(ctx, {
+      msg: "Server Error",
+      status: 500,
+    });
+  }
+  if (!user) {
+    return Response.notFound(ctx, {
+      msg: "Your password is wrong",
+      status: 400,
+    });
+  }
+  try {
+    await strapi.services.customer.updatePassword(email, newPassword);
+  } catch (error) {
+    return Response.internalServerError(ctx, {
+      msg: "Server Error",
+      status: 500,
+    });
+  }
+  return Response.ok(ctx, {
+    msg: "Updated password successfully!",
+    data: newPassword,
+    status: 200,
+  });
+};
+
 module.exports = {
+  changePassword: changePassword,
+  resetPassWord: resetPassWord,
   signup: signUp,
   login: logIn,
+  findOneByEmail: findOneByEmail,
 };
