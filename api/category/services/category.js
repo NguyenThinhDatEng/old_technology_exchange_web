1caffe81882ff4cbc8ea0accc20a7a8d035cfedb
NguyenThinhDatSci <thinh.nv194178@sis.hust.edu.vn>
2022-01-09 12:28:47 +0700

    add findOneByTitle

--- api/category/services/category.js
+++ api/category/services/category.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
- * to customize this service
- */
+const findOneByTitle = async (title) => {
+  return strapi.query("category").findOne({ title });
+};
 
-module.exports = {};
+module.exports = { findOneByTitle };
